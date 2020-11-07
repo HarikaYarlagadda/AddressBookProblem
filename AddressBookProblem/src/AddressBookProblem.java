933b5aa2b51ff8cf8a027ba8409ce2942e840edb
HarikaYarlagadda <hariyhari1999@gmail.com>
2020-11-07 13:55:35 +0530

    Added Regular expression for zip and phoneNumber

--- AddressBookProblem/src/AddressBookProblem.java
+++ AddressBookProblem/src/AddressBookProblem.java
@@ -10,18 +10,19 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+
 public class AddressBookProblem {
 
 		Scanner scanner = new Scanner(System.in);
 
-		public static void main(String[] args) {
+		public static void main(String[] args) throws Exception {
 			System.out.println("Welcome to address book problem");
 			AddressBookProblem addressBook = new AddressBookProblem();
 			addressBook.askUser();
 
 	}
 
-void askUser() {
+void askUser() throws Exception {
 	System.out.println("Select an action..");
 	System.out.println("1. To Create a new contact");
 	System.out.println("2. To fill details of contact");
@@ -98,13 +99,13 @@ private void fillDetailsOfContact() {
 	    @SuppressWarnings("resource")
 		Scanner sc= new Scanner(System.in); 
 	   
-	    System.out.print("\nEnter firstname:");  
-	    String firstname= sc.nextLine();
-	    System.out.println("\nEnter lastname");
-        String lastname = sc.nextLine();  
-	    System.out.print("\nYou entered firstname and lastname: "+firstname +lastname);   
-		bufferedWriter.write( firstname);
-        bufferedWriter.write( lastname );
+	    System.out.print("\nEnter firstName:");  
+	    String firstName= sc.nextLine();
+	    System.out.println("\nEnter lastName");
+        String lastName = sc.nextLine();  
+	    System.out.print("\nYou entered firstname and lastname: "+firstName +lastName);   
+		bufferedWriter.write( firstName);
+        bufferedWriter.write( lastName );
         System.out.print("\nEnter a address: ");
         String address= sc.nextLine();
         System.out.print("\nYou entered address: "+address);
@@ -114,17 +115,41 @@ private void fillDetailsOfContact() {
 	    System.out.print("\nEnter state:");
         String state=sc.nextLine();
         System.out.print("\nEnter zip:");
-        String zip=sc.nextLine();                
+        String zip;
+        while(true) {
+        	zip= sc.nextLine();                
+		    String zipRegex = "^[0-9]{6}$";
+		      if( zip.matches(zipRegex))
+				  {
+		    	  System.out.print("\nYou entered zip: "+zip);
+				  break;
+				  }
+		     else
+		    	  System.out.println("Entered zip is invalid");
+        }
+        
         System.out.print("\n entered city: "+city);
         bufferedWriter.write(city.toString());
         System.out.print("\nYou entered state: "+state); 
 	    bufferedWriter.write( state.toString());
 	    System.out.print("\nYou entered zip: "+zip); 
         bufferedWriter.write( zip.toString());
-	    System.out.print("\nEnter phonenumber: ");  
-	    String phonenumber= sc.nextLine();                 
-	    System.out.print("\nYou entered phonenumber: "+phonenumber);   
-		bufferedWriter.write( phonenumber.toString() );
+	    System.out.print("\nEnter phoneNumber: ");  
+	    String phoneNumber;
+	    while(true) 
+		   {
+		    System.out.print("\nEnter phoneNumber: ");  
+		    phoneNumber= sc.nextLine();              //reads string  
+		    String phoneNumberRegex = "^(\\d{2}[ ]){1}\\d{8}$";
+		      if( phoneNumber.matches(phoneNumberRegex))
+		      {
+				  System.out.print("\nYou entered phoneNumber: "+phoneNumber);   
+				  break;
+		      }
+		      else
+		    	  System.out.println("Entered mobile number is invalid");
+		   }   
+		bufferedWriter.write( phoneNumber.toString() );
 	    bufferedWriter.close();
 	    System.out.println("\nDone");
 	} catch(IOException e) {
@@ -167,75 +192,72 @@ private void deleteFile() {
 	        }
 	
 }
-private void editDetails()  
+private void editDetails() throws Exception 
+{
+	System.out.print("Enter contact name to read: ");
+	@SuppressWarnings("resource")
+	Scanner scanner = new Scanner(System.in);
+	String contactName = scanner.next();
+	System.out.println("\nYou choose to EDIT");
+	System.out.println("Press 1 to EDIT Address");
+	System.out.println("Press 2 to EDIT firstname");
+	System.out.println("Press 3 to EDIT secondname");
+	System.out.println("Press 4 to EDIT city");
+	System.out.println("Press 5 to EDIT state");
+	System.out.println("Press 6 to EDIT zip");
+	System.out.println("Press 7 to EDIT phonenumber");
+
+	int choice = scanner.nextInt();
+	switch(choice) 
 	{
-		System.out.print("Enter file name to read: ");
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		String fileName = scanner.next();
-		System.out.println("\nYou choose to EDIT");
-		System.out.println("Press 1 to EDIT Address");
-		System.out.println("Press 2 to EDIT firstname");
-		System.out.println("Press 3 to EDIT secondname");
-		System.out.println("Press 4 to EDIT city");
-		System.out.println("Press 5 to EDIT state");
-		System.out.println("Press 6 to EDIT zip");
-		System.out.println("Press 7 to EDIT phonenumber");
-
-		int choice = scanner.nextInt();
-		switch(choice) 
-		{
-		case 1:
-			editoptions(choice,fileName); 
-            break;
-		case 2:
-			editoptions(choice,fileName); 
-            break;
-		case 3:
-			editoptions(choice,fileName); 
-            break;
-		case 4:
-			editoptions(choice,fileName); 
-            break;
-		case 5:
-			editoptions(choice,fileName); 
-            break;
-		case 6:
-			editoptions(choice,fileName); 
-            break;
-		case 7:
-			editoptions(choice,fileName); 
-            break;
-	}
-  }
-	 public static void setVariable(int lineNumber, String data,String fileName) throws IOException 
-	 {
-			
-		    Path path = Paths.get("D:Addressbookof"+fileName);
-		    java.util.List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-		    lines.set(lineNumber - 1, data);
-		    Files.write(path, lines, StandardCharsets.UTF_8);
-	 }
-
-	void editoptions(int choice,String fileName) 
-	 {
-		    @SuppressWarnings("resource")
-			Scanner scanner = new Scanner(System.in);
-		    System.out.print("\n Enter text :");
-			String Text = scanner.next();
-			try
-			{
-			@SuppressWarnings("unused")
-			File obj =new File("D:addressbookof"+fileName);
-		    setVariable(choice, Text,fileName);
-		    System.out.println("Done");
-			} 
-			catch(IOException e)
-			{
-						    System.out.println("COULD NOT EDIT FILE");
-						
-			}
-	 }
-
+	case 1:
+		editoptions(choice,contactName); 
+        break;
+	case 2:
+		editoptions(choice,contactName); 
+        break;
+	case 3:
+		editoptions(choice,contactName); 
+        break;
+	case 4:
+		editoptions(choice,contactName); 
+        break;
+	case 5:
+		editoptions(choice,contactName); 
+        break;
+	case 6:
+		editoptions(choice,contactName); 
+        break;
+	case 7:
+		editoptions(choice,contactName); 
+        break;
 }
-
+}
+ public static void setVariable(int lineNumber, String data,String contactName) throws IOException 
+ {
+		
+	    Path path = Paths.get("D:addressbookof"+contactName);
+	    java.util.List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+	    lines.set(lineNumber - 1, data);
+	    Files.write(path, lines, StandardCharsets.UTF_8);
+ }
+ void editoptions(int choice,String contactName) 
+ {
+	    @SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+	    System.out.print("\n Enter text :");
+		String Text = scanner.next();
+		try
+		{
+		@SuppressWarnings("unused")
+		File obj =new File("D:addressbookof"+contactName);
+	    setVariable(choice, Text,contactName);
+	    System.out.println("Done");
+		} 
+		catch(IOException e)
+		{
+		    System.out.println("COULD NOT EDIT FILE");
+					
+		}
+ }
+}
